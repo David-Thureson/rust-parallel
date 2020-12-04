0b7a991fd97c865adcfda204b20a179fb6515bf5
David Thureson <David.G.Thureson@gmail.com>
2020-12-03 17:58:26 -0800

    Added the polygon method for Pi.

--- src/monte_carlo/pi.rs
+++ src/monte_carlo/pi.rs
@@ -1,26 +1,29 @@
 use std::ops::Range;
 use std::time::{Instant, Duration};
 use rand::prelude::*;
+use std::f64::consts::PI;
 //use std::f64::consts::PI;
 
 const PI_DIGITS: &str = "14159265358979323846264338327950288";
 
 pub fn main() {
-    monte_carlo_serial(15, 10, 10);
+    // monte_carlo_serial(15, 10, 10);
+    // polygon_range(3..100);
+    polygon_exponential(20, 10, 10);
 }
 
 #[derive(Debug)]
 struct PiResult {
-    pub dart_count: usize,
+    pub count: usize,
     pub duration: Duration,
     pub correct_digits: usize,
     pub approx_pi: f64,
 }
 
 impl PiResult {
-    pub fn new(dart_count: usize, duration: Duration, correct_digits: usize, approx_pi: f64) -> Self {
+    pub fn new(count: usize, duration: Duration, correct_digits: usize, approx_pi: f64) -> Self {
         Self {
-            dart_count,
+            count,
             duration,
             correct_digits,
             approx_pi,
@@ -57,6 +60,35 @@ fn monte_carlo_serial(trial_count: usize, darts_from: usize, darts_mult: usize) 
         dart_count *= darts_mult;
     }
     // bg!(&overall_results);
+}
+
+fn polygon_increment(sides_range: Range<usize>) {
+    for side_count in sides_range {
+        polygon_one(side_count);
+    }
+}
+
+fn polygon_exponential(trial_count: usize, sides_from: usize, sides_mult: usize) {
+    let mut side_count = sides_from;
+    for _ in 0..trial_count {
+        polygon_one(side_count);
+        side_count *= sides_mult;
+    }
+}
+
+fn polygon_one(side_count: usize) {
+    let radius = 1.0;
+    let start = Instant::now();
+    let side_count_f64 = side_count as f64;
+    let angle = (2.0 * PI) / (side_count_f64 * 2.0);
+    let s = angle.sin();
+    let approx_circumference = s * side_count_f64 * 2.0;
+    let approx_pi = approx_circumference / 2.0;
+    //bg!(side_count_f64, angle.to_degrees(), s, approx_circumference, approx_pi);
+    let duration = Instant::now() - start;
+    let correct_digits = count_correct_pi_digits(approx_pi);
+    let result = PiResult::new(side_count, duration, correct_digits, approx_pi);
+    dbg!(&result);
 }
 
 fn count_correct_pi_digits(approx_pi: f64) -> usize {
